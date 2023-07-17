4505d4b62316e0540e67be4375c7d637d16c15a3
Nikhil Pandey <nikhilpandey4@gmail.com>
2023-07-17 07:17:26 +0530

    Went back to My App stage.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
 function App() {
-    const title = 'Blog Post';
-    const body = 'My awesome blog post for the world to see!';
-    const blogPosts = [
-        { 'id': 1, 'text': 'Blog Post one.' },
-        { 'id': 2, 'text': 'Blog Post two.' },
-        { 'id': 3, 'text': 'Blog Post three.' }
-    ];
-    const showBlogs = false;
-    const showLoading = <h1>Loading...</h1>;
-    if (showBlogs) return (showLoading);
-    const showComments = true;
-    const commentsBlock = (< div className="comments" >
-        <p>Blog Posts count ({blogPosts.length})</p>
-        <h3>My blog posts are</h3>
-        <h3>Total comments ({blogPosts.length})</h3>
-        <ul>
-            {blogPosts.map((blog, index) => (
-                <li key={index}>{blog.text}</li>
-            ))}
-        </ul>
-    </div >);
+
 
     return (
         <div className="container">
-            <h1>{title}</h1>
-            <p>{body}</p>
-
-            ({showComments ? commentsBlock : showLoading})
+            <h1>My App</h1>
 
         </div>
     )
