ee1fde59b9c1d2894e42fdd1ea9059d1a55f2f38
Nikhil Pandey <nikhilpandey4@gmail.com>
2023-07-17 07:06:43 +0530

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const title = 'Blog Post';
+    const body = 'My awesome blog post for the world to see!';
+    const blogPosts = [
+        { 'id': 1, 'text': 'Blog Post one.' },
+        { 'id': 2, 'text': 'Blog Post two.' },
+        { 'id': 3, 'text': 'Blog Post three.' }
+    ];
+    const showBlogs = false;
+    const showLoading = <h1>Loading...</h1>;
+    if (showBlogs) return (showLoading);
+    const showComments = true;
+    const commentsBlock = (< div className="comments" >
+        <p>Blog Posts count ({blogPosts.length})</p>
+        <h3>My blog posts are</h3>
+        <h3>Total comments ({blogPosts.length})</h3>
+        <ul>
+            {blogPosts.map((blog, index) => (
+                <li key={index}>{blog.text}</li>
+            ))}
+        </ul>
+    </div >);
+
+    return (
+        <div className="container">
+            <h1>{title}</h1>
+            <p>{body}</p>
+
+            ({showComments ? commentsBlock : showLoading})
+
+        </div>
+    )
 }
 
 export default App;
