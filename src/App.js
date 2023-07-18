fb4e066a1ce9f6e3391d1794240dd4e6b5d08077
Nikhil Pandey <nikhilpandey4@gmail.com>
2023-07-18 12:57:54 +0530

    Completed css adding in react.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import Header from "./components/Header";
 function App() {
 
 
     return (
-        <div className="container">
-            <h1>My App</h1>
 
+        <div>
+            <Header/>
+            <h1>My App</h1>
         </div>
+
+
     )
 }
 
