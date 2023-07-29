d6a83733bd5ad578e55887c3ba03cc595ef2f20a
Nikhil Pandey <nikhilpandey4@gmail.com>
2023-07-29 17:14:02 +0530

    Completed feedback items.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
+
 function App() {
+  const msg = "Can you make it look like this clipart i found i'll know it when i see it, so can we have another option"
   return (
     <>
       <Header />
-      <div className="container">
-        <h1>My App!</h1>
-      </div>
+      <FeedbackItem rating={8} text={msg}/>
     </>
   );
 }
