35cdf3ed7a76e3db79340f5db8e0ef1e370ad47f
Nikhil Pandey <nikhilpandey4@gmail.com>
2023-07-29 09:09:40 +0530

    Added to style additions.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import Header from "./components/Header";
 function App() {
-
-
-    return (
-
-        <div>
-            <Header/>
-            <h1>My App</h1>
-        </div>
-        
-
-
-
-    )
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>My App!</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
